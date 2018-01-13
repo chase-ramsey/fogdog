70997d44445768071a0eea58c31973696bc9aac6
Chase Ramsey <ramsey.chase@gmail.com>
2018-01-13 15:07:11 -0600

    Allow for event/context params

--- app/handler.py
+++ app/handler.py
@@ -2,9 +2,10 @@ from fogdog import Fogdog
 from config import Config, create_logger
 
 
-def go_get_em_fogdog():
+def go_get_em_fogdog(event, context):
 	Config.check_env()
 
 	logger = create_logger()
+
 	dog = Fogdog(logger)
 	dog.fetch()
